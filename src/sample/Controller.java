54decabdde64e7650bc840f112501a53a78c78ec
sher <sher@os.os.com>
2020-03-18 20:23:00 +0500

    Testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        label.setText("Hello World!");
+    }
+
+
 }

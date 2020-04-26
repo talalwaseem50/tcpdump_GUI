b12da4c8ddddff3a2d0edb923bc70939c3c0e665
jrvs98 <mfarzaibkhan786@gmail.com>
2020-04-26 19:33:23 +0500

    tokeniztion done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -24,7 +25,7 @@ public class Controller {
 
     //private Protocol p1 = new Protocol("HTTP");
     ArrayList<Protocol> protocolList;
-
+    ArrayList<Packet> packetList = null;
     public void initialize() {
         label.setText("Initialized");
 
@@ -33,6 +34,11 @@ public class Controller {
         pNameList.add("SMTP");
         pNameList.add("VOIP");
         pNameList.add("FTP");
+        pNameList.add("DNS");
+        pNameList.add("POP3");
+        pNameList.add("TELNET");
+        pNameList.add("NTP");
+
 
         protocolList = new ArrayList<Protocol>();
 
@@ -44,20 +50,22 @@ public class Controller {
             lTemp.textProperty().bind(pTemp.valueProperty());
             protocolBox.getChildren().add(lTemp);
         }
-
-        TableColumn dataColumn = (TableColumn) packetTable.getColumns().get(0);//new TableColumn("Temp");
-        dataColumn.setCellValueFactory(new PropertyValueFactory<>("data"));
-
+        String packetComponents[] = {"time","IP","length","srcAddress","destAddress","headerData"};
+        for(int i = 0; i < packetTable.getColumns().size(); i++) {
+            TableColumn dataColumn = (TableColumn) packetTable.getColumns().get(i);//new TableColumn("Temp");
+            dataColumn.setCellValueFactory(new PropertyValueFactory<>(packetComponents[i]));
+        }
 
     }
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
+
         label.setText("Running!");
-        protocolList.get(0).increment();
 
         MultithreadedRun mRun = new MultithreadedRun();
         mRun.start();
+
     }
 
     /*  --------------------------------------------------------------------- */
@@ -67,20 +75,73 @@ public class Controller {
         {
             try
             {
+                packetList = new ArrayList<Packet>();
                 String s;
                 Process p;
-                p = Runtime.getRuntime().exec("sudo /usr/sbin/tcpdump -c 5");
+                p = Runtime.getRuntime().exec("sudo /usr/sbin/tcpdump -c 5 -vv");
                 BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
                 while ((s = br.readLine()) != null) {
-                    //System.out.println(s);
-                    Packet pTemp = new Packet(s);
+                    System.out.println(s);
+                    StringTokenizer tokenizer = new StringTokenizer(s);
+                    String time = tokenizer.nextToken(" ");
+                    String IP = tokenizer.nextToken(" ");
+                    String length = null;
+                    String headerData = null;
+                    String srcAddress = null;
+                    String destAddress = null;
+                    String packetData = null;
+                    String appProtocol = null;
+                    if(!s.contains("IP6"))
+                    {
+                        headerData = tokenizer.nextToken(")").toString().concat("))");
+                        length = tokenizer.nextToken(")");
+                        length = length.substring(2);
+                        s = br.readLine();
+                        s = s.substring(4);
+                        tokenizer = new StringTokenizer(s);
+                        srcAddress = tokenizer.nextToken(">");
+                        destAddress = tokenizer.nextToken(":");
+                        destAddress = destAddress.substring(2);
+                        packetData = tokenizer.nextToken("\n");
+                        System.out.println(s);
+                    }else
+                    {
+                        headerData = tokenizer.nextToken(")").toString().concat("))");
+                        length = tokenizer.nextToken(")");
+                        srcAddress = tokenizer.nextToken(">");
+                        destAddress = tokenizer.nextToken(":");
+                        destAddress = destAddress.substring(2);
+
+                    }
+
+                    Packet pTemp = new Packet(time,length,IP,headerData,srcAddress,destAddress,packetData);
                     packetTable.getItems().add(pTemp);
+                    packetList.add(pTemp);
                 }
 
                 p.waitFor();
                 System.out.println ("exit: " + p.exitValue());
                 Platform.runLater(() -> {
+                            for(int i = 0; i < packetList.size(); i++) {
+                                if (packetList.get(i).getSrcAddress().contains("http") || packetList.get(i).getDestAddress().contains("http")) {
+                                    protocolList.get(0).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("smtp") || packetList.get(i).getDestAddress().contains("smtp")) {
+                                    protocolList.get(1).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("voip") || packetList.get(i).getDestAddress().contains("voip")) {
+                                    protocolList.get(2).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("ftp") || packetList.get(i).getDestAddress().contains("ftp")) {
+                                    protocolList.get(3).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("domain") || packetList.get(i).getDestAddress().contains("domain") || packetList.get(i).getSrcAddress().contains("dns") || packetList.get(i).getDestAddress().contains("dns")) {
+                                    protocolList.get(4).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("25") || packetList.get(i).getDestAddress().contains("25")) {
+                                    protocolList.get(5).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("telnet") || packetList.get(i).getDestAddress().contains("telnet")) {
+                                    protocolList.get(6).increment();
+                                } else if (packetList.get(i).getSrcAddress().contains("ntp") || packetList.get(i).getDestAddress().contains("ntp")) {
+                                    protocolList.get(6).increment();
+                                }
+                            }
                     label.setText("Done!");
                 });
                 p.destroy();
--- src/sample/Packet.java
+++ src/sample/Packet.java
@@ -1,18 +1,88 @@
 package sample;
 
+import java.util.StringTokenizer;
+
 public class Packet {
 
-    private String data;
+    private String time;
+    private String length;
+    private String IP;
+    private String headerData;
+    private String srcAddress;
+    private String destAddress;
+    private String packetData;
 
-    Packet(String d) {
-        data = d;
+    public String getTime() {
+        return time;
     }
 
-    public String getData() {
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    public String getHeaderData() {
+        return headerData;
+    }
+
+    public void setHeaderData(String headerData) {
+        this.headerData = headerData;
+    }
+
+    public String getSrcAddress() {
+        return srcAddress;
+    }
+
+    public void setSrcAddress(String srcAddress) {
+        this.srcAddress = srcAddress;
+    }
+
+    public String getDestAddress() {
+        return destAddress;
+    }
+
+    public void setDestAddress(String destAddress) {
+        this.destAddress = destAddress;
+    }
+
+    public String getPacketData() {
+        return packetData;
+    }
+
+    public void setPacketData(String packetData) {
+        this.packetData = packetData;
+    }
+
+    Packet(String t, String l, String i, String h, String s, String de, String p) {
+        time = t;
+        length = l;
+        IP = i;
+        headerData = h;
+        srcAddress = s;
+        destAddress = de;
+        packetData = p;
+    }
+
+    /*public String getData() {
         return data;
     }
 
     public void setData(String data) {
         this.data = data;
-    }
+    }*/
 }
